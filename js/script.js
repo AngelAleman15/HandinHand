160434b3712eeac416ab3c4dcd9de59c10f9cf6e
Alejosantos007 <alejosantos.2024@gmail.com>
2025-06-23 20:26:51 -0300

    Intento de realizar el form de registro con JS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@ const open_sidebar = document.getElementById('open_sidebar');
 const icon_sidebar = document.getElementById('icon_sidebar');
 const menu_sidebar = document.getElementById('menu_sidebar');
 const images_menu = document.querySelectorAll('menu_sidebar img');
+let account = [];
+let user_name = document.getElementById('name');
+let user_surname = document.getElementById('surname');
+let password = document.getElementById('password');
+let phone = document.getElementById('phone');
+let email = document.getElementById('email');
+
 
 open_sidebar.addEventListener('click', sidebar);
 open_sidebar.addEventListener('mouseleave', () => {
@@ -16,4 +23,12 @@ function sidebar() {
     icon_sidebar.style = 'display: none';   
     menu_sidebar.style = 'display:flex';
     open_sidebar.className = 'sidebar_active';
+}
+
+function register(account) {
+    user_name = name.value;
+    user_surname = surname.value;
+    password = password.value;
+    email = email.value;
+    phone = phone.value; 
 }
