84d605a0d309638f9587811a9d91d7c1bb5fcf3c
Alejosantos007 <alejosantos.2024@gmail.com>
2025-06-24 19:42:29 -0300

    JS arreglito

--- js/register.js
+++ js/register.js
@@ -2,19 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById("btn-registrar");
 
     if (btn) {
-        btn.addEventListener("click", () => {
-            const user = {
-                name: document.getElementById("name").value,
-                surname: document.getElementById("surname").value,
-                password: document.getElementById("password").value,
-                email: document.getElementById("email").value,
-                phone: document.getElementById("phone").value
-            };
+    btn.addEventListener("click", () => {
+        const name = document.getElementById("name").value.trim();
+        const surname = document.getElementById("surname").value.trim();
+        const password = document.getElementById("password").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const phone = document.getElementById("phone").value.trim();
 
+        if (name && surname && password && email && phone) {
+            const user = { name, surname, password, email, phone };
             localStorage.setItem("registeredUser", JSON.stringify(user));
-            alert("Usuario registrado con éxito ");
-        });
-    }
+            alert("Usuario registrado con éxito");
+        } else {
+            alert("Por favor, agregá toda la información.");
+        }
+    });
+}
+
     document.getElementById("name").value = "";
     document.getElementById("surname").value = "";
     document.getElementById("email").value = "";
